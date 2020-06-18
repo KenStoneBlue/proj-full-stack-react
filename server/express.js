d5b7c9f0189295e5bfc76405a342b9055848eaa9
Ken Stone <ken@stoneblue.com>
2020-06-18 15:44:13 -0500

    server-side rendering

--- server/express.js
+++ server/express.js
@@ -9,6 +9,16 @@ import Template from './../template'
 import userRoutes from './routes/user.routes'
 import authRoutes from './routes/auth.routes'
 
+// modules for server side rendering
+import React from 'react'
+import ReactDOMServer from 'react-dom/server'
+import MainRouter from './../client/MainRouter'
+import { StaticRouter } from 'react-router-dom'     // import StaticRouter from 'react-router-dom/StaticRouter
+
+import { ServerStyleSheets, ThemeProvider } from '@material-ui/styles'
+import theme from './../client/theme'
+//end
+
 //comment out before building for production
 import devBundle from './devBundle'
 
@@ -18,6 +28,7 @@ const app = express()
 //comment out before building for production
 devBundle.compile(app)
 
+// parse body params and attache them to req.body
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
@@ -33,8 +44,26 @@ app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
 app.use('/', userRoutes)
 app.use('/', authRoutes)
 
-app.get('/', (req, res) => {
-    res.status(200).send(Template())
+app.get('*', (req, res) => {
+  const sheets = new ServerStyleSheets()
+  const context = {}
+  const markup = ReactDOMServer.renderToString(
+    sheets.collect(
+          <StaticRouter location={req.url} context={context}>
+            <ThemeProvider theme={theme}>
+              <MainRouter />
+            </ThemeProvider>
+          </StaticRouter>
+        )
+    )
+    if (context.url) {
+      return res.redirect(303, context.url)
+    }
+    const css = sheets.toString()
+    res.status(200).send(Template({
+      markup: markup,
+      css: css
+    }))
 })
 
 // Catch unauthorised errors
