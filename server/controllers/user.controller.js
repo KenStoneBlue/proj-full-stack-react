527295e498970accb85a9cd5fd35144e493ddb8e
Ken Stone <ken@stoneblue.com>
2020-07-29 14:20:10 -0500

    adding the shop functionality WIP

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,9 +1,11 @@
 import User from '../models/user.model'
 import extend from 'lodash/extend'
 import errorHandler from './../helpers/dbErrorHandler'
-import formidable from 'formidable'
-import fs from 'fs'
-import profileImage from './../../client/assets/images/profile-pic.png'
+import request from 'request'
+import config from './../../config/config'
+import stripe from 'stripe'
+
+const myStripe = stripe(config.stripe_test_secret_key)
 
 const create = async (req, res) => {
   const user = new User(req.body)
@@ -24,9 +26,7 @@ const create = async (req, res) => {
  */
 const userByID = async (req, res, next, id) => {
   try {
-    let user = await User.findById(id).populate('following', '_id name')
-    .populate('followers', '_id name')
-    .exec()
+    let user = await User.findById(id)
     if (!user)
       return res.status('400').json({
         error: "User not found"
@@ -57,35 +57,20 @@ const list = async (req, res) => {
   }
 }
 
-//const update = async (req, res) => {
-const update = (req, res) => {
-  let form = new formidable.IncomingForm()
-  form.keepExtensions = true
-  form.parse(req, async (err, fields, files) => {
-    if (err) {
-      return res.status(400).json({
-        error: "Photo could not be uploaded"
-      })
-    }
+const update = async (req, res) => {
+  try {
     let user = req.profile
-    user = extend(user, fields)
+    user = extend(user, req.body)
     user.updated = Date.now()
-    if(files.photo){
-      user.photo.data = fs.readFileSync(files.photo.path)
-      user.photo.contentType = files.photo.type
-    }
-    try {
-      await user.save()
-      user.hashed_password = undefined
-      user.salt = undefined
-      res.json(user)
-    } catch (err) {
-      console.log('ERROR: ' + errorHandler.getErrorMessage(err));
-      return res.status(400).json({
-        error: errorHandler.getErrorMessage(err)
-      })
-    }
-  })
+    await user.save()
+    user.hashed_password = undefined
+    user.salt = undefined
+    res.json(user)
+  } catch (err) {
+    return res.status(400).json({
+      error: errorHandler.getErrorMessage(err)
+    })
+  }
 }
 
 const remove = async (req, res) => {
@@ -102,94 +87,89 @@ const remove = async (req, res) => {
   }
 }
 
-const photo = (req, res, next) => {
-  if(req.profile.photo.data){
-    res.set("Content-Type", req.profile.photo.contentType)
-    return res.send(req.profile.photo.data)
-  }
-  next()
-}
-
-const isEducator = (req, res, next) => {
-  const isEducator = req.profile && req.profile.educator
-  if (!isEducator) {
+const isSeller = (req, res, next) => {
+  const isSeller = req.profile && req.profile.seller
+  if (!isSeller) {
     return res.status('403').json({
-      error: "User is not an educator"
+      error: "User is not a seller"
     })
   }
   next()
 }
 
-const defaultPhoto = (req, res) => {
-  return res.sendFile(process.cwd()+profileImage)
-}
-
-const addFollowing = async (req, res, next) => {
-  try{
-    await User.findByIdAndUpdate(req.body.userId, {$push: {following: req.body.followId}}) 
-    next()
-  }catch(err){
-    return res.status(400).json({
-      error: errorHandler.getErrorMessage(err)
-    })
-  }
-}
-
-const addFollower = async (req, res) => {
-  try{
-    let result = await User.findByIdAndUpdate(req.body.followId, {$push: {followers: req.body.userId}}, {new: true})
-                            .populate('following', '_id name')
-                            .populate('followers', '_id name')
-                            .exec()
-      result.hashed_password = undefined
-      result.salt = undefined
-      res.json(result)
-    }catch(err) {
-      return res.status(400).json({
-        error: errorHandler.getErrorMessage(err)
+const stripe_auth = (req, res, next) => {
+  request({
+    url: "https://connect.stripe.com/oauth/token",
+    method: "POST",
+    json: true,
+    body: {client_secret:config.stripe_test_secret_key,code:req.body.stripe, grant_type:'authorization_code'}
+  }, (error, response, body) => {
+    //update user
+    if(body.error){
+      return res.status('400').json({
+        error: body.error_description
       })
-    }  
+    }
+    req.body.stripe_seller = body
+    next()
+  })
 }
 
-const removeFollowing = async (req, res, next) => {
-  try{
-    await User.findByIdAndUpdate(req.body.userId, {$pull: {following: req.body.unfollowId}}) 
-    next()
-  }catch(err) {
-    return res.status(400).json({
-      error: errorHandler.getErrorMessage(err)
-    })
-  }
-}
-const removeFollower = async (req, res) => {
-  try{
-    let result = await User.findByIdAndUpdate(req.body.unfollowId, {$pull: {followers: req.body.userId}}, {new: true})
-                            .populate('following', '_id name')
-                            .populate('followers', '_id name')
-                            .exec() 
-    result.hashed_password = undefined
-    result.salt = undefined
-    res.json(result)
-  }catch(err){
-      return res.status(400).json({
-        error: errorHandler.getErrorMessage(err)
+const stripeCustomer = (req, res, next) => {
+  if(req.profile.stripe_customer){
+      //update stripe customer
+      myStripe.customers.update(req.profile.stripe_customer, {
+          source: req.body.token
+      }, (err, customer) => {
+        if(err){
+          return res.status(400).send({
+            error: "Could not update charge details"
+          })
+        }
+        req.body.order.payment_id = customer.id
+        next()
+      })
+  }else{
+      myStripe.customers.create({
+            email: req.profile.email,
+            source: req.body.token
+      }).then((customer) => {
+          User.update({'_id':req.profile._id},
+            {'$set': { 'stripe_customer': customer.id }},
+            (err, order) => {
+              if (err) {
+                return res.status(400).send({
+                  error: errorHandler.getErrorMessage(err)
+                })
+              }
+              req.body.order.payment_id = customer.id
+              next()
+            })
       })
   }
 }
 
-const findPeople = async (req, res) => {
-  let following = req.profile.following
-  following.push(req.profile._id)
-  try {
-    let users = await User.find({ _id: { $nin : following } }).select('name')
-    console.log('user.controller.js: findPeople: users:');
-    console.log(users);
-    res.json(users)
-  }catch(err){
-    return res.status(400).json({
-      error: errorHandler.getErrorMessage(err)
+const createCharge = (req, res, next) => {
+  if(!req.profile.stripe_seller){
+    return res.status('400').json({
+      error: "Please connect your Stripe account"
     })
   }
+  myStripe.tokens.create({
+    customer: req.order.payment_id,
+  }, {
+    stripeAccount: req.profile.stripe_seller.stripe_user_id,
+  }).then((token) => {
+      myStripe.charges.create({
+        amount: req.body.amount * 100, //amount in cents
+        currency: "usd",
+        source: token.id,
+      }, {
+        stripeAccount: req.profile.stripe_seller.stripe_user_id,
+      }).then((charge) => {
+        next()
+      })
+  })
 }
 
 export default {
@@ -199,12 +179,8 @@ export default {
   list,
   remove,
   update,
-  photo,
-  isEducator,
-  defaultPhoto,
-  addFollowing,
-  addFollower,
-  removeFollowing,
-  removeFollower,
-  findPeople
+  isSeller,
+  stripe_auth,
+  stripeCustomer,
+  createCharge
 }
